3604b3b9df1e20cbea4a0024d8245e3661e8cd87
Cleanlight-sys <alandownad@gmail.com>
2025-08-05 18:31:10 -0400

    Update Cleanlight_bk.py

--- Cleanlight_bk.py
+++ Cleanlight_bk.py
@@ -1,3 +1,4 @@
+print("App started — top of file", flush=True)
 from flask import Flask, request, jsonify
 import requests
 import os
@@ -247,5 +248,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 

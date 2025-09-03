67e93715ad5111ee949f358a77af67122c3b7bfa
Cleanlight-sys <alandownad@gmail.com>
2025-09-03 17:28:20 -0400

    Update build.py

--- schema/build.py
+++ schema/build.py
@@ -2,10 +2,12 @@
 from .base import base
 from .paths_query import get as query_paths
 from .paths_hint import get as hint_paths
+from .paths_schema_get import get as schema_get_paths
 
 def build_spec():
     spec = base.copy()
     spec["paths"] = {}
     spec["paths"].update(query_paths())
     spec["paths"].update(hint_paths())
+    spec["paths"].update(schema_get_paths())
     return spec
